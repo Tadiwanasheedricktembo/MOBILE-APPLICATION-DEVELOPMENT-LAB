2ce468229bc080b4902c834b019a163978a813e2
tadiwanasheedricktembo <tadiwanasheedricktembo@gmail.com>
2026-09-23 00:07:35 +0530

    Add remaining MAD LAB projects and Gradle files

---
+++ IMPLICITINTENT/app/src/main/java/com/tadiwatembo/implicitintent/MainActivity.java
@@ -0,0 +1,54 @@
+package com.tadiwatembo.implicitintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button btnSms = findViewById(R.id.btn_sms);
+        Button btnUrl = findViewById(R.id.btn_url);
+        Button btnDial = findViewById(R.id.btn_dial);
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("smsto:0712345678"));
+                intent.putExtra("sms_body", "Hello from Implicit Intent!");
+                startActivity(intent);
+            }
+        });
+
+        btnUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0712345678"));
+                startActivity(intent);
+            }
+        });
+    }
+}
